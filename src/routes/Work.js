4b243487a91a2912bc0174624ad5db16aa16c01b
Ruby <ruby0322@ntu.im>
2023-08-25 03:48:20 +0800

    Removed Redundant Code

--- src/routes/Work.js
+++ src/routes/Work.js
@@ -1,26 +1,10 @@
 import '@fontsource/noto-serif-tc';
 import { Typography } from '@mui/material';
-import { useParams } from 'react-router-dom';
 import ImageListPreview from '../components/ImageListPreview';
 import Profile from '../components/Profile';
 import WorkSubtitle from '../components/WorkSubtitle';
 
-const images = [
-    'https://wallpaperaccess.com/full/275808.jpg',
-    'https://th.bing.com/th/id/R.909bebe9f717ecf8a127ef72ba52460a?rik=23LSklVldsDgNQ&riu=http%3a%2f%2fwkidk.sarpat.com%2fimg%2fi5085.jpg&ehk=O73Z8iaY4UlSYJ0oP9qW%2fCRNm37%2byUvIiFbXMDSF7o4%3d&risl=&pid=ImgRaw&r=0',
-    'https://ohspets.shelterbuddy.com/photos/lostfound/69344.jpg',
-    'https://th.bing.com/th/id/R.786c2293ee14a86d8c35221a64f5d2ca?rik=ublinbaiWSbq3Q&riu=http%3a%2f%2f3.bp.blogspot.com%2f_Ym3du2sG3R4%2fTM3up8uIR_I%2fAAAAAAAAC7A%2fn8IwOAhlt7k%2fs1600%2fCat-wallpaper.jpg&ehk=IvSHZY1NgbQ4Ba%2fIs%2fqyu1%2bIRQzoYmd%2bMNyTowKRzEs%3d&risl=&pid=ImgRaw&r=0',
-    'https://th.bing.com/th/id/R.b98c753daf8a260a9d9bb79f34d8a56b?rik=sGBwzyUNoSAJmA&pid=ImgRaw&r=0',
-    'https://th.bing.com/th/id/OIP.PxyXw81RuPMA7WJ9w4FWrQAAAA?pid=ImgDet&rs=1',
-    'https://i.pinimg.com/originals/ca/90/14/ca90149f7cb77f17dbabf930c755bcb3.jpg',
-    'https://i.pinimg.com/originals/09/20/ce/0920ce97932744b69c95453e3735d373.jpg',
-    'https://th.bing.com/th/id/R.2a234fbe706eebacdc9752ccffb1e35d?rik=AJv4agoEDFVVPw&pid=ImgRaw&r=0',
-    'https://th.bing.com/th/id/OIP.S-E3-g11OOEhIYSbpXGp4wHaNJ?pid=ImgDet&w=676&h=1200&rs=1',
-];
-
 const Work = () => {
-
-    const { workId } = useParams();
 
     return <div className='paper-container'>
         <div style={{
